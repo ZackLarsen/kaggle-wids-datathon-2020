239912c402111934fae674a5dcc078434d247875
ZackLarsen <zlarsen4@gmail.com>
2023-10-07 12:01:49 -0500

    Runs Prefect flow from script

--- src/kaggle_prefect_flow.py
+++ src/kaggle_prefect_flow.py
@@ -1,100 +1,117 @@
-# """
-# Use Prefect to orchestrate the Kaggle competition pipeline.
-# """
+"""
+Use Prefect to orchestrate the Kaggle competition pipeline.
+"""
 
-# from prefect import flow, task
-# import polars as pl
-# import pandas as pd
-# import mlflow
-# from sklearn.model_selection import train_test_split
-# from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-# import xgboost as xgb
-
-
-# @task
-# def ingest(path):
-#     data = pl.read_csv(path)
-#     return data
+from pathlib import Path
+from prefect import flow, get_run_logger
+import polars as pl
+import polars.selectors as cs
+import pandas as pd
+import mlflow
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+import xgboost as xgb
 
 
-# @task
-# def calc_mean_age(data):
-#     return data.select("age").mean()
+data_path = "/Users/zacklarsen/Documents/Projects/kaggle-wids-datathon-2020/data/"
+training_v2_path = Path(data_path, "training_v2.csv")
 
 
-# # @task
-# # def transform(data):
-# #     # Your code for data transformation
-# #     return transformed_data
+@flow
+def ingest(path):
+    data = pl.read_csv(path, infer_schema_length=10000, dtypes={"age": pl.Float32}, ignore_errors=True)
+    logger = get_run_logger()
+    logger.info("Data ingested")
+
+    return data
 
 
-# @task
-# def split(data, test_size=0.2, random_state=42):
-#     X = data.drop('hospital_death')
-#     y = data['hospital_death']
+@flow
+def calc_mean_age(data):
+    mean_age = data.select(pl.col("age")).mean().item()
+    logger = get_run_logger()
+    logger.info(f"Mean age calculated as: {mean_age}")
 
-#     X_train, X_test, y_train, y_test = train_test_split(
-#         X, y, test_size=test_size, random_state=random_state)
-
-#     return X_train, X_test, y_train, y_test
+    return mean_age
 
 
-# @task
-# def save_splits(X_train, X_test, y_train, y_test):
-#     X_train.to_parquet('data/X_train.parquet')
-#     y_train.to_parquet('data/y_train.parquet')
-#     X_test.to_parquet('data/X_test.parquet')
-#     y_test.to_parquet('data/y_test.parquet')
+@flow
+def split(data, test_size=0.2, random_state=42):
+    logger = get_run_logger()
+    logger.info("Splitting data into train and test sets")
+    X = data.drop('hospital_death')
+    y = data['hospital_death']
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state)
+
+    logger.info("Size of train set: {X_train.shape}")
+    logger.info("Size of test set: {X_test.shape}")
+
+    return X_train, X_test, y_train, y_test
 
 
-# @task
-# def train_model(X_train, y_train):
-#     model = xgb.XGBClassifier()
-#     model.fit(X_train, y_train)
-#     return model
+@flow
+def save_splits(X_train, X_test, y_train, y_test):
+    logger = get_run_logger()
+    logger.info("Saving splits")
+    X_train.write_parquet(Path(data_path, 'X_train.parquet'))
+    pl.DataFrame(y_train).write_parquet(Path(data_path, 'y_train.parquet'))
+    X_test.write_parquet(Path(data_path, 'X_test.parquet'))
+    pl.DataFrame(y_test).write_parquet(Path(data_path, 'y_test.parquet'))
+
+    return None
 
 
-# @task
-# def predict(model, test_data):
-#     preds = model.predict(test_data)
-#     return preds
+@flow
+def train_model(X_train, y_train):
+    logger = get_run_logger()
+    logger.info("Training XGBoost classifier")
+    model = xgb.XGBClassifier()
+    model.fit(X_train, y_train)
+
+    return model
 
 
-# @task
-# def evaluate_model(y_test, y_pred):
-#     accuracy = accuracy_score(y_test, y_pred)
-#     precision = precision_score(y_test, y_pred)
-#     recall = recall_score(y_test, y_pred)
-#     f1 = f1_score(y_test, y_pred)
-#     roc_auc = roc_auc_score(y_test, y_pred)
+@flow
+def predict(model, test_data):
+    logger = get_run_logger()
+    logger.info("Predicting on test data")
+    preds = model.predict(test_data)
 
-#     print(f"Accuracy: {accuracy:.4f}")
-#     print(f"Precision: {precision:.4f}")
-#     print(f"Recall: {recall:.4f}")
-#     print(f"F1 score: {f1:.4f}")
-#     print(f"ROC AUC score: {roc_auc:.4f}")
-
-#     return accuracy, precision, recall, f1, roc_auc
+    return preds
 
 
-# with flow("WiDS2020") as flow:
-#     data = ingest('data/training_v2.csv')
-#     mean_age = calc_mean_age(data)
-#     # transformed_data = transform(data)
-#     X_train, X_test, y_train, y_test = split(data)
-#     save_splits(X_train, X_test, y_train, y_test)
-#     model = train_model(X_train, y_train)
-#     preds = predict(model, X_test)
+@flow
+def evaluate_model(y_test, y_pred):
+    logger = get_run_logger()
+    logger.info("Evaluating model performance")
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    roc_auc = roc_auc_score(y_test, y_pred)
 
-#     accuracy, precision, recall, f1, roc_auc = evaluate_model(y_test, preds)
+    return accuracy, precision, recall, f1, roc_auc
 
-#     mlflow.set_experiment("first_xgb_clf")
-#     with mlflow.start_run():
-#         mlflow.log_param("model_type", "XGBoost")
-#         mlflow.log_metric("accuracy", accuracy)
-#         mlflow.log_metric("precision", precision)
-#         mlflow.log_metric("recall", recall)
-#         mlflow.log_metric("f1", f1)
-#         mlflow.log_metric("roc_auc", roc_auc)
 
-#     flow.run()
+@flow
+def prep_workflow(file_path):
+    data = ingest(file_path)
+    mean_age = calc_mean_age(data)
+    X_train, X_test, y_train, y_test = split(data, test_size=0.2, random_state=42)
+    save_splits(X_train, X_test, y_train, y_test)
+    clf = train_model(X_train.select(cs.numeric()), y_train)
+    y_pred = predict(clf, X_test.select(cs.numeric()))
+    accuracy, precision, recall, f1, roc_auc = evaluate_model(y_test, y_pred)
+
+    result_dict = {
+        'mean_age': mean_age,
+        'accuracy': accuracy,
+        'precision': precision,
+        'recall': recall,
+        'f1': f1,
+        'roc_auc': roc_auc
+    }
+
+    return result_dict
